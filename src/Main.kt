182cf68c148cca449f9178671ed1ad1f5b3e858a
k-seito <torinokonoko@gmail.com>
2020-08-25 20:29:06 +0900

    add capitalize property

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
-fun main() {
-    println("Hello Functional Kotlin!")
+fun main(args: Array<String>) {
+    println(capitalize("hello world!"))
+}
+
+val capitalize = object: Function1<String, String> {
+    override fun invoke(p1: String): String {
+        return p1.capitalize()
+    }
 }
