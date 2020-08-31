393e4865f8cacc365f039cee7656f3117c7fa0dc
k-seito <torinokonoko@gmail.com>
2020-08-31 18:06:40 +0900

    Update Main.kt
    end of Immutable reference

--- src/chapter3/what_is_immutability/Main.kt
+++ src/chapter3/what_is_immutability/Main.kt
@@ -4,6 +4,16 @@ fun main() {
     for (i in 1..3) {
         println("Calling ${i}st time ${MutableVal.myString}")
     }
+
+    val mutableObj: MutableObj = MutableObj()
+    println("MutableObj $mutableObj")
+    mutableObj.value = "Changed"
+    println("MutableObj $mutableObj")
+
+    val list = mutableListOf("a", "b", "c", "d", "e")
+    println(list)
+    list.add("f")
+    println(list)
 }
 
 object MutableVal {
@@ -12,4 +22,12 @@ object MutableVal {
         get() {
             return "$field ${++count}"
         }
+}
+
+class MutableObj {
+    var value = ""
+
+    override fun toString(): String {
+        return "MutableObj(value=$value)"
+    }
 }
